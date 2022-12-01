9b38c44af372e9dc0a9123c3cb5eeffd6cb4b4da
신범호 <shinn0608@naver.com>
2022-12-01 17:14:24 +0900

    css작업

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ import DatePicker from "react-datepicker";
 import { ko } from 'date-fns/esm/locale';
 import "react-datepicker/dist/react-datepicker.css";
 import "./react-datepicker.css"
-import starImg from '../images/start.jpg';
+import starImg from '../images/star.jpg';
 
 function Home() {
     /* 스크롤 이동 */
@@ -38,7 +38,7 @@ function Home() {
 
         console.log(typeof objectiveName);
         console.log(typeof objectiveDate);
-        alert(`"${objectiveName}" 목표가 생성되었습니다. ${objectiveDate}`);      //템플릿 문자열 
+        alert(`"${objectiveName}" 목표가 생성되었습니다.`);      //템플릿 문자열 
 
         //submit 후 input 제거 하기
         setInput({
@@ -74,21 +74,23 @@ function Home() {
             </ButtonDiv>
             <ObjectiveList objectives={objectives}></ObjectiveList>
             <FormDiv ref={inputForm}>
-                <form onSubmit={onSubmit}>
-                    <h3>Input Form 들어갈곳</h3>
-                    <label>
-                        *목표명: <input type="text" name="objectiveName" value={objectiveName} onChange={onChange} />
-                    </label><br /><br />
-                    <div>
-                        *목표 달성일 입력: <DatePicker name="objectiveDate" selected={objectiveDate} isClearable autoComplete='off' 
-                            minDate={new Date()} // 과거 날짜 disable
-                            onChange={(date) => setInput({ ...input, "objectiveDate": date })} locale={ko} dateFormat="yyyy년 MM월 dd일"
-                            dayClassName={date => getDayName(createDate(date)) === '일' ? "sunday" : undefined}
-                        />
-                    </div>
-                    <br />
-                    <button>제출</button>
-                </form>
+                <Form onSubmit={onSubmit}>
+                    <FormTitle>有備無患 : 유비무환</FormTitle>
+                    
+                    <InputDiv>
+                        <h2> 목표 입력하기 </h2>
+                        <Label> 목표명 </Label><br/>
+                        <Input type="text" name="objectiveName" value={objectiveName} onChange={onChange} /><br /><br />
+                        <Label> 목표 달성일 </Label>
+                        <DatePicker name="objectiveDate" selected={objectiveDate} isClearable autoComplete='off' 
+                                minDate={new Date()} // 과거 날짜 disable
+                                onChange={(date) => setInput({ ...input, "objectiveDate": date })} locale={ko} dateFormat="yyyy년 MM월 dd일"
+                                dayClassName={date => getDayName(createDate(date)) === '일' ? "sunday" : undefined}
+                            />
+                    </InputDiv>
+                    <br/>
+                    <SubmitBtn>제출</SubmitBtn>
+                </Form>
             </FormDiv>
         </HomeBody>
     );
@@ -97,7 +99,7 @@ function Home() {
 export default Home;
 
 const HomeBody = styled.div`
-    font-family : sans-serif;
+    font-family : Noto Sans KR,sans-serif;
     text-align : center;
 `
 const HomeDiv = styled.div`
@@ -107,14 +109,13 @@ const HomeDiv = styled.div`
 `
 
 const ButtonDiv = styled.div`
-    height: 5vh;
+    height: 7vh;
     position: relative;
     overflow: hidden;
 `
 
-
 const TitleH1 = styled.h1`
-    font-family: 'Montserrat', sans-serif;
+    font-family: Noto Sans KR,sans-serif;
     font-size: 90px;
     font-weight: 900;
     position: absolute;
@@ -131,14 +132,93 @@ const TitleH1 = styled.h1`
 `
 
 const AddBtn = styled.button`
-    
+    box-shadow : 0px 2px 10px -5px #000000;
+    border: none;
+    border-radius: 20px;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    padding-left: 5rem;
+    padding-right: 5rem;
+    height: 2.25rem;
+    font-size: 1rem;
+    background: #21223C;
+    &:hover {
+      background: #3F416E;
+    }
+    &:active {
+      background: #21223C;
+    }
 `
 
 const FormDiv = styled.div`
     height: 100vh;
-    font-size: 20px;
-    font-weight: bold;
     background: #e9ecef;
     display: flex;
     justify-content : center;
+`
+
+const Form = styled.form`
+    height: 600px;
+    width: 500px;
+    margin-top: 80px;
+    background : white;
+    border: 0px solid;
+    border-radius: 10px;
+    box-shadow : 0px 2px 8px -5px #000000;
+    outline: none;
+    overflow: hidden;
+`;
+
+const FormTitle = styled.div`
+    background : #21223C; 
+    padding: 20px;
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+    
+`;
+
+const InputDiv = styled.div`
+    padding-top: 20px;
+    padding-left: 20%;
+    text-align: left;
+    font-size: 17px;
+    font-weight: bold;
+`
+
+const Label = styled.label`
+    color: #595959;
+    line-height: 1.5rem;
+`;
+
+
+const Input = styled.input`
+    width: 300px;
+    border: 1px solid gray;
+    border-radius: 5px;
+    outline: none;
+    line-height: 2.5rem;
+    font-size: 1.2rem;
+`;
+
+
+const SubmitBtn = styled.button`
+    width: 305px;
+    margin-left: 5px;
+    box-shadow : 0px 2px 10px -5px #000000;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    line-height: 2.5rem;
+    font-size: 1rem;
+    background: #21223C;
+    &:hover {
+      background: #3F416E;
+    }
+    &:active {
+      background: #21223C;
+    }
 `
